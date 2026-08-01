6b816937cf5b778b6a9204bbf4f2848e65591df3
Sjler <info@b4s.co.za>
2026-08-01 15:42:20 +0200

    Harden restore pin backfill eligibility

--- os2-preview/migrations/20260801_025_merge_authorisation_restore_pin.sql
+++ os2-preview/migrations/20260801_025_merge_authorisation_restore_pin.sql
@@ -8,6 +8,12 @@ SET a.restore_test_id=(
   FROM os2_restore_tests rt
   WHERE rt.backup_run_id=a.backup_run_id
     AND rt.status='passed'
+    AND rt.completed_at IS NOT NULL
+    AND rt.target_environment='isolated_preview_restore'
+    AND rt.expected_database_name='kloka_talk2me'
+    AND rt.actual_database_name='kloka_talk2me'
+    AND rt.failed_checks=0
+    AND (a.authorised_at IS NULL OR rt.completed_at<=a.authorised_at)
   ORDER BY rt.completed_at DESC,rt.id DESC
   LIMIT 1
 )
