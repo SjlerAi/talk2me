81146e512c2aa1680d32da6c259d53b0ecd0480e
Sjler <info@b4s.co.za>
2026-09-11 14:48:29 +0200

    Register monthly source-evidence refresh

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ router.use(require('./routes/contact-list-builder'));
 router.use(require('./routes/customer-details'));
 router.use(require('./routes/staff-import-identities'));
 router.use(require('./routes/base-details-centre'));
+router.use(require('./routes/monthly-source-evidence-refresh'));
 router.use(require('./routes/index'));
 
 module.exports = router;
