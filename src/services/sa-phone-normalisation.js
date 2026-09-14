28aa5729e4b3737f0df89f1689404df9af374202
Sjler <info@b4s.co.za>
2026-09-14 08:07:27 +0200

    Promote proven South African phone normalization

--- src/services/sa-phone-normalisation.js
+++ src/services/sa-phone-normalisation.js
@@ -10,8 +10,5 @@ const MOBILE_PHONE_FIELDS = Object.freeze([
 
 function normaliseSouthAfricanMobile(value) {
   let digits = String(value ?? '').trim().replace(/^\+/, '').replace(/\D/g, '');
-  if (/^0\d{9}$/.test(digits)) digits = `27${digits.slice(1)}`;
-  return /^27[6-8]\d{8}$/.test(digits) ? digits : '';
-}
 
-module.exports = { normaliseSouthAfricanMobile, MOBILE_PHONE_FIELDS };
+  // Be forgiving with imported numbers that
